ba1338064dd18d6bbf083a2b96df9c1a9b323e83
okoropaulc <okoropaulc@yahoo.com>
2018-09-27 13:35:28 -0500

    Metstry1 files

---
+++ Third/mets1rscript.R
@@ -0,0 +1,130 @@
+library(dada2)
+path <- "/home/paul/mets1"
+list.files(path)
+
+# Forward and reverse fastq filenames have format: SAMPLENAME_R1_001.fastq and SAMPLENAME_R2_001.fastq
+fnFs <- sort(list.files(path, pattern="_R1_001.fastq", full.names = TRUE))
+fnRs <- sort(list.files(path, pattern="_R2_001.fastq", full.names = TRUE))
+# Extract sample names, assuming filenames have format: SAMPLENAME_XXX.fastq
+sample.names <- sapply(strsplit(basename(fnFs), "_"), `[`, 1)
+#Examine quality profiles of forward and reverse reads
+
+#plotQualityProfile(fnFs[1:4])
+#plotQualityProfile(fnRs[1:4])
+
+#Perform filtering and trimming
+filt_path <- file.path(path, "filtered") # Place filtered files in filtered/ subdirectory
+filtFs <- file.path(filt_path, paste0(sample.names, "_F_filt.fastq.gz"))
+filtRs <- file.path(filt_path, paste0(sample.names, "_R_filt.fastq.gz"))
+#Filter the forward and reverse reads
+out <- filterAndTrim(fnFs, filtFs, fnRs, filtRs, truncLen=c(275,175), maxN=0, matchIDs = TRUE, maxEE=c(2,2), truncQ=2, rm.phix=TRUE, compress=TRUE, multithread=TRUE)
+head(out)
+
+##Examine quality profiles of filtered forward and reverse reads
+#pdf("/home/paul/metstry1/Second/qplotTrimmedF.pdf")
+plotQualityProfile(filtFs[1:4])
+#dev.off()
+#pdf("/home/paul/metstry1/Second/qplotTrimmedR.pdf")
+plotQualityProfile(filtRs[1:4])
+#dev.off()
+
+#Learn the Error rates
+errF <- learnErrors(filtFs, multithread=TRUE)
+errR <- learnErrors(filtRs, multithread=TRUE)
+plotErrors(errF, nominalQ=TRUE)
+
+#Depreplication
+derepFs <- derepFastq(filtFs, verbose=TRUE)
+derepRs <- derepFastq(filtRs, verbose=TRUE)
+# Name the derep-class objects by the sample names
+names(derepFs) <- sample.names
+names(derepRs) <- sample.names
+
+#Sample Inference
+#We are now ready to apply the core sequence-variant inference algorithm to the dereplicated data.
+#Infer the sequence variants in each sample
+dadaFs <- dada(derepFs, err=errF, multithread=TRUE)
+dadaRs <- dada(derepRs, err=errR, multithread=TRUE)
+#Inspecting the dada-class object returned by dada:
+dadaFs[[1]]
+#head(dadaFs[[1]]$clustering)
+#head(dadaRs[[1]]$clustering)
+
+#Merge Paired reads
+#Merge the denoised forward and reverse reads:
+mergers <- mergePairs(dadaFs, derepFs, dadaRs, derepRs, justConcatenate = TRUE, verbose=TRUE)
+#mergePairs(ddF, drpF, ddR, drpR, minOverlap=4)
+# Inspect the merger data.frame from the first sample
+head(mergers[[1]])
+
+#Construct the sequence table
+#The sequence table is a matrix with rows corresponding to (and named by) the samples, and columns corresponding to (and named by) the sequence variants
+seqtab <- makeSequenceTable(mergers)
+dim(seqtab)
+# Inspect distribution of sequence lengths
+table(nchar(getSequences(seqtab)))
+#Remove Chimeric Sequence
+seqtab.nochim <- removeBimeraDenovo(seqtab, method="consensus", multithread=TRUE, verbose=TRUE)
+dim(seqtab.nochim)
+
+#Track reads through the pipeline
+#As a final check of our progress, we’ll look at the number of reads that made it through each step in the pipeline:
+getN <- function(x) sum(getUniques(x))
+track <- cbind(out, sapply(dadaFs, getN), sapply(mergers, getN), rowSums(seqtab), rowSums(seqtab.nochim))
+colnames(track) <- c("input", "filtered", "denoised", "merged", "tabled", "nonchim")
+rownames(track) <- sample.names
+head(track)
+write.table(track, file ="/home/paul/metstry1/Third/mergersReport.txt", sep="\t", quote = F)
+
+#Assign Taxonomy
+taxa <- assignTaxonomy(seqtab.nochim, "/home/paul/silva_nr_v128_train_set.fa.gz", multithread=TRUE)
+
+#Because I used justConcatenate=TRUE (which do not allow species to be assigned), I have to write a code to specially addSpecie to the sequences
+asvSeqs <- gsub("N{10}[A-z]*", "", getSequences(seqtab.nochim))
+asgnSpecs <- assignSpecies(asvSeqs, "/home/paul/silva_species_assignment_v128.fa.gz")
+
+# transfer species annotation to same genus ASVs else "NA"
+same.genus <- asgnSpecs[,"Genus"] == taxa[,"Genus"]
+same.genus[is.na(same.genus)] <- FALSE
+taxa <- cbind(taxa, Species = rep(NA, nrow(taxa)))
+taxa[same.genus, "Species"] <- asgnSpecs[same.genus, "Species"]
+head(taxa[same.genus, "Species"])
+
+#sq.R1 <- substr(getSequences(seqtab.nochim), 1, R1_READ_LEN)
+#gen.spc <- assignSpecies(sq.R1, "/home/paul/silva_species_assignment_v128.fa.gz")
+
+#taxa <- addSpecies(taxa, "/home/paul/silva_species_assignment_v128.fa.gz")
+#Lets inspect the taxonomic assignments
+taxa.print <- taxa # Removing sequence rownames for display only
+rownames(taxa.print) <- NULL
+head(taxa.print) #Prints the taxonomy with species
+
+
+#Extracting the result from R to generate a Fasta file of ASV, a count table and a taxonomy table
+# giving our seq headers more manageable names (ASV_1, ASV_2...)
+asv_seqs <- colnames(seqtab.nochim)
+asv_headers <- vector(dim(seqtab.nochim)[2], mode="character")
+
+for (i in 1:dim(seqtab.nochim)[2]) {
+  asv_headers[i] <- paste(">ASV", i, sep="_")
+}
+
+# making and writing out a fasta of our final ASV seqs:
+asv_fasta <- c(rbind(asv_headers, asv_seqs))
+write(asv_fasta, "/home/paul/metstry1/Third/ASVs.fa")
+
+# count table:
+asv_tab <- t(seqtab.nochim)
+row.names(asv_tab) <- sub(">", "", asv_headers)
+write.table(asv_tab, "/home/paul/metstry1/Third/ASVs_counts.txt", sep="\t", quote=F)
+
+# taxa table with no species assignment:
+asv_tax <- taxa
+row.names(asv_tax) <- sub(">", "", asv_headers)
+write.table(asv_tax, "/home/paul/metstry1/Third/ASVs_taxonomy.txt", sep="\t", quote=F)
+
+# taxa table with species:
+asv_tax <- taxa.print
+row.names(asv_tax) <- sub(">", "", asv_headers)
+write.table(asv_tax, "/home/paul/metstry1/Third/ASVs_taxonomy_with_Species.txt", sep="\t", quote=F)
+
